dcc2243d8dee611fb556f6f29e890cb2a00abffa
kousaatsu <121098273+kousaatsu@users.noreply.github.com>
2023-05-01 16:28:09 +0300

    static jumping

--- Hospital/Assets/Scripts/PlayerMovement.cs
+++ Hospital/Assets/Scripts/PlayerMovement.cs
@@ -22,11 +22,6 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
 
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y);
-        }
-
         Flip();
     }
 
